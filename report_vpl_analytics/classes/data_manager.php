ab9625138cd2523493b4ad8bc275a138eb3220c9
pablobll <pablo.ezmon@gmail.com>
2026-06-23 23:52:24 +0100

    Backend: implementing ETL engine in data_manager, mixing vpl and vpl_submissions tables

--- report_vpl_analytics/classes/data_manager.php
+++ report_vpl_analytics/classes/data_manager.php
@@ -27,35 +27,61 @@ class data_manager {
         return $data;
     }
 
-    public static function get_student_metrics() {
+    public static function get_dashboard_data() {
+        $vpls = self::get_vpl_data();
         $submissions = self::get_submissions_data();
-        $metrics = [];
+
+        $vpl_course_map = [];
+        $vpl_name_map = [];
+        foreach ($vpls as $v) {
+            $vpl_course_map[$v['id']] = $v['course'];
+            $vpl_name_map[$v['id']] = $v['name'];
+        }
+
+        $enriched_submissions = [];
+        $unique_courses = [];
+        $unique_groups = [];
+        $unique_users = [];
 
         foreach ($submissions as $sub) {
-            $userid = $sub['userid'];
-            if (!isset($metrics[$userid])) {
-                $metrics[$userid] = [
-                    'userid' => $userid,
-                    'total_submissions' => 0,
-                    'total_runs' => 0,
-                    'total_debugs' => 0,
-                    'average_grade' => 0,
-                    'sum_grades' => 0
-                ];
+            $vpl_id = isset($sub['vpl']) ? $sub['vpl'] : null;
+            if (!$vpl_id) continue;
+
+            $course = isset($vpl_course_map[$vpl_id]) ? $vpl_course_map[$vpl_id] : 'Desconocido';
+            $vpl_name = isset($vpl_name_map[$vpl_id]) ? $vpl_name_map[$vpl_id] : 'VPL ' . $vpl_id;
+            $group = isset($sub['groupid']) && $sub['groupid'] !== '' ? (int)$sub['groupid'] : 0; 
+            $user = $sub['userid'];
+
+            if($course !== 'Desconocido') {
+                $unique_courses[$course] = true;
             }
-            
-            $metrics[$userid]['total_submissions'] += 1;
-            $metrics[$userid]['total_runs'] += (int)$sub['run_count'];
-            $metrics[$userid]['total_debugs'] += (int)$sub['debug_count'];
-            $metrics[$userid]['sum_grades'] += (float)$sub['grade'];
+            $unique_groups[$group] = true;
+            $unique_users[$user] = true;
+
+            $enriched_submissions[] = [
+                'id' => $sub['id'],
+                'vpl' => $vpl_id,
+                'vpl_name' => $vpl_name,
+                'course' => $course,
+                'userid' => $user,
+                'groupid' => $group,
+                'datesubmitted' => (int)$sub['datesubmitted'],
+                'grade' => (float)$sub['grade'],
+                'run_count' => (int)$sub['run_count'],
+                'debug_count' => (int)$sub['debug_count'],
+                'nevaluations' => (int)$sub['nevaluations']
+            ];
         }
 
-        foreach ($metrics as &$m) {
-            if ($m['total_submissions'] > 0) {
-                $m['average_grade'] = round($m['sum_grades'] / $m['total_submissions'], 2);
-            }
-        }
+        $courses = array_keys($unique_courses); sort($courses);
+        $groups = array_keys($unique_groups); sort($groups);
+        $users = array_keys($unique_users); sort($users);
 
-        return $metrics;
+        return [
+            'courses' => $courses,
+            'groups' => $groups,
+            'users' => $users,
+            'submissions' => $enriched_submissions
+        ];
     }
 }
