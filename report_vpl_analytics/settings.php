4b801b9f2dc74ce6dea574cda23af3ef46b850ed
pablobll <pablo.ezmon@gmail.com>
2026-07-05 13:08:32 +0100

    Fix moodle routing and settings page

--- report_vpl_analytics/settings.php
+++ report_vpl_analytics/settings.php
@@ -1,11 +1,12 @@
 <?php
 defined('MOODLE_INTERNAL') || die;
 
-if ($hassiteconfig) {
-    $ADMIN->add('reports', new admin_externalpage(
-        'reportvplanalytics',
-        get_string('pluginname', 'report_vpl_analytics'),
-        new moodle_url('/report/vpl_analytics/index.php'),
-        'report/vpl_analytics:view'
-    ));
-}
+$ADMIN->add('reports', new admin_externalpage(
+    'reportvpl_analytics',
+    get_string('pluginname', 'report_vpl_analytics'),
+    new moodle_url('/report/vpl_analytics/index.php'),
+    'report/vpl_analytics:view'
+));
+
+$settings = new admin_settingpage('reportvpl_analytics_settings', get_string('pluginname', 'report_vpl_analytics'));
+$settings->add(new admin_setting_heading('reportvpl_analytics_heading', '', 'Este plugin no requiere configuración técnica. Por favor, navega a la pestaña de "Informes" (Reports) en la Administración del sitio para acceder al Dashboard interactivo.'));
